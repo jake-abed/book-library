a7d809397196eb7b4f284231f6f3e21af1c793df
jake-abed <jakeabed@gmail.com>
2023-01-28 12:25:40 -0800

    Start structuring JS, add card & button styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,35 @@
 'use strict';
 
-const dog = {
-	woof: 'Woof!'
+//Global object to store any settings if necessary.
+
+
+//Declare display object to store display elements
+
+const display = {
+	addBookButton: document.querySelector('.add-book'),
+	libraryWrapper: document.querySelector('.library-wrapper')
 }
 
-console.log(dog.woof);
+//Create library to store books.
+
+const bookLibrary = [];
+
+//Create book constructor.
+
+function Book(title, author, pages, haveRead = false) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.haveRead = haveRead;
+}
+
+//Create a function to add book to the library.
+
+const addBookToLibrary = (book) => bookLibrary.push(book);
+
+//Create function that loops over the library and adds all books to display.
+
+const populateLibraryWrapper = () => {
+	const libraryWrapper = display.libraryWrapper;
+	const bookCard = document.createElement('div',)
+}
